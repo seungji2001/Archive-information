9196f83184972193a7ba4f5f4feabc7f9bbbdbd1
Seungji Baek <seungjibaek@Seungjis-MacBook-Pro.local>
2023-10-19 02:14:22 +0900

    humanParsingAPI controller 연결

--- src/main/java/com/example/demo/Controller/OpenAPIController.java
+++ src/main/java/com/example/demo/Controller/OpenAPIController.java
@@ -1,24 +1,22 @@
-//package com.example.demo.Controller;
-//
-//import com.example.demo.Component.OpenAPIManager;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.io.UnsupportedEncodingException;
-//
-//import static jdk.vm.ci.hotspot.HotSpotCompilationRequestResult.success;
-//
-//@RestController
-//@RequiredArgsConstructor
-//@Slf4j
-//public class OpenAPIController {
-//    private final OpenAPIManager openApiManager;
-//
-//    @GetMapping("open-api")
-//    public ResponseEntity<?> fetch() throws UnsupportedEncodingException {
-//        return success(openApiManager.fetch().getBody());
-//    }
-//}
+package com.example.demo.Controller;
+
+import com.example.demo.Component.OpenAPIManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class OpenAPIController {
+    private final OpenAPIManager openApiManager;
+
+    @GetMapping("open-api")
+    public void fetch() throws UnsupportedEncodingException {
+        openApiManager.getHumanParsingApi();
+    }
+}
