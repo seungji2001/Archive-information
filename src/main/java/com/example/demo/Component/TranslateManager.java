967686c56e6fc9d508dc6e155b4242afe19f3c9a
Seungji Baek <seungjibaek@Seungjis-MacBook-Pro.local>
2023-11-09 05:00:38 +0900

    translate api 추가

---
+++ src/main/java/com/example/demo/Component/TranslateManager.java
@@ -0,0 +1,133 @@
+package com.example.demo.Component;
+
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+
+    // 네이버 기계번역 (Papago SMT) API 예제
+    public class TranslateManager {
+//        public static void main(String[] args) {
+//            String clientId = "5V3ZWI6y0PKAAYWOYSno";
+//            String clientSecret = "u7OtyNxnNn";
+//            String apiURL = "https://openapi.naver.com/v1/papago/n2mt";
+//            String text;
+//            text = URLEncoder.encode("안녕하세요. 오늘 기분은 어떻습니까?", StandardCharsets.UTF_8);
+//
+//            Map<String, String> requestHeaders = new HashMap<>();
+//            requestHeaders.put("X-Naver-Client-Id", clientId);
+//            requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+//
+//            String responseBody = post(apiURL, requestHeaders, text);
+//
+//            System.out.println(responseBody);
+//        }
+//
+//        private static String post(String apiUrl, Map<String, String> requestHeaders, String text) {
+//            HttpHeaders headers = new HttpHeaders();
+//            headers.setAll(requestHeaders);
+//
+//            MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+//            body.add("source", "ko");
+//            body.add("target", "en");
+//            body.add("text", text);
+//
+//            HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(body, headers);
+//
+//            RestTemplate restTemplate = new RestTemplate();
+//
+//            ResponseEntity<String> responseEntity = restTemplate.exchange(apiUrl, HttpMethod.POST, requestEntity, String.class);
+//
+//            if (responseEntity.getStatusCode() == HttpStatus.OK) {
+//                System.out.println(requestEntity.getBody());
+//                return responseEntity.getBody();
+//            } else {
+//                // Handle error response
+//                return null;
+//            }
+//        }
+
+        public void translate(String clientId, String clientSecret, String korean) throws MalformedURLException {
+            String apiURL = "https://openapi.naver.com/v1/papago/n2mt";
+            String text;
+            try {
+                text = URLEncoder.encode(korean, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException("인코딩 실패", e);
+            }
+
+            Map<String, String> requestHeaders = new HashMap<>();
+            requestHeaders.put("X-Naver-Client-Id", clientId);
+            requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+
+            String responseBody = post(apiURL, requestHeaders, text);
+
+            System.out.println(responseBody);
+        }
+
+        private static String post(String apiUrl, Map<String, String> requestHeaders, String text) throws MalformedURLException {
+            HttpURLConnection con = connect(apiUrl);
+            String postParams = "source=ko&target=en&text=" + text; //원본언어: 한국어 (ko) -> 목적언어: 영어 (en)
+            try {
+                con.setRequestMethod("POST");
+                for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+                    con.setRequestProperty(header.getKey(), header.getValue());
+                }
+
+                con.setDoOutput(true);
+                try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+                    wr.write(postParams.getBytes());
+                    wr.flush();
+                }
+
+                int responseCode = con.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 응답
+                    return readBody(con.getInputStream());
+                } else {  // 에러 응답
+                    return readBody(con.getErrorStream());
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("API 요청과 응답 실패", e);
+            } finally {
+                con.disconnect();
+            }
+        }
+
+        private static HttpURLConnection connect(String apiUrl) throws MalformedURLException {
+            try {
+                URL url = new URL(apiUrl);
+                return (HttpURLConnection)url.openConnection();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private static String readBody(InputStream body){
+            InputStreamReader streamReader = new InputStreamReader(body);
+
+            try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+                StringBuilder responseBody = new StringBuilder();
+
+                String line;
+                while ((line = lineReader.readLine()) != null) {
+                    responseBody.append(line);
+                }
+
+                return responseBody.toString();
+            } catch (IOException e) {
+                throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
+            }
+        }
+    }
+
+
+
--- src/main/java/com/example/demo/Controller/DataArchiveController.java
+++ src/main/java/com/example/demo/Controller/DataArchiveController.java
@@ -1,6 +1,7 @@
 package com.example.demo.Controller;
 
 import com.example.demo.Component.NELinkingManager;
+import com.example.demo.Component.TranslateManager;
 import com.example.demo.Dto.DataArchiveDto.DataArchiveRequestDto;
 import com.example.demo.Dto.DataArchiveDto.DataArchiveResponseDto;
 import com.example.demo.Dto.NELinking.NELinkingRequestDto;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.xml.crypto.Data;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +27,8 @@ public class DataArchiveController {
     DataArchiveService dataArchiveService;
     @Autowired
     NELinkingManager neLinkingManager;
+    @Autowired
+    TranslateManager translateManager;
     @Value("${api.key}")
     private String getKey;
 
@@ -32,6 +36,10 @@ public class DataArchiveController {
     String googleKey;
     @Value("${cx}")
     String cx;
+    @Value("${client.id}")
+    String client_id;
+    @Value("${secret.key}")
+    String secret_key;
 
     @PostMapping("/write/data")
     public ResponseEntity<Long> writeData(@RequestBody DataArchiveRequestDto.WriteData writeData){
@@ -100,5 +108,10 @@ public class DataArchiveController {
         String contents = "노벨 물리학상( - 物理學賞, 스웨덴어: Nobelpriset i fysik, 영어: Nobel Prize in Physics)은 6개 분야의 노벨상 중 하나로, 1년에 한 번 스웨덴 왕립 과학원에 의해 수여된다. 첫 번째 노벨 물리학상은 1901년, 엑스선을 발견한 독일의 빌헬름 콘라트 뢴트겐에게 수여되었다. 이 상은 노벨 재단이 주관하며, 이 상을 수상하는 것은 물리학계에서 최고의 영예로 꼽힌다. 노벨 물리학상은 알프레드 노벨의 사망일인 12월 10일에 스톡홀름에서 수여된다. 2007년의 노벨 물리학상은 프랑스의 알베르 페르와 독일의 페터 그륀베르크가 거대 자기저항의 발견에 대한 공로로 공동 수상하였고, 상금인 1천만 스웨덴 크로나를 나누어 가졌다. 노벨 물리학상은 사람들이 그 과학자의 업적의 중요성을 알기까지의 시간이 걸리기 때문에, 다른 한 편으로는 시간과의 싸움이라고 말할 수 있다. 예를 들어 1983년 노벨 물리학상 수상자인 수브라마니안 찬드라세카르의 경우 그 이론은 이미 1930년에 이미 발표하였지만 사람들에게 인정을 받지 못하여 50여년이 지나서야 상을 받을 수가 있었다. 또한 2013년 노벨 물리학상 수상자인 피터 힉스와 프랑수아 앙글레르의 경우 그의 이론을 검증할 수 있게 되는 과학기술이 발전하기 까지 오랜 시간이 걸려, 49년 뒤 그의 업적이 사실로 확인되어 노벨상을 받게 되었다. 그래서 많은 이론과 발견이 사람들에게 중요성을 인정받았지만, 그 이론이나 발견을 발표한 과학자가 이미 죽어버렸기에 노벨 물리학상을 받지 못하는 안타까운 경우도 있다.";
 
         return ResponseEntity.ok().body(neLinkingManager.getNeLinking(getKey, requestDto.getContent()));
+    }
+
+    @GetMapping("/translate")
+    public void getTranslate() throws MalformedURLException {
+        translateManager.translate(client_id, secret_key, "안녕");
     }
 }
